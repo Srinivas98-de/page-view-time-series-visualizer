3d90ac5fbe2510fb3a10853b5c139da2e8c0c0f6
Srinivas98-de <srinivas982902@gmail.com>
2025-08-01 14:49:43 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# coding: utf-8
+
+# In[4]:
+
+
+# Run this cell to import your visualizer functions
+import time_series_visualizer as ts
+
+# Draw and display the line plot
+fig1 = ts.draw_line_plot()
+fig1.show()
+
+# Draw and display the bar plot
+fig2 = ts.draw_bar_plot()
+fig2.show()
+
+# Draw and display the box plots
+fig3 = ts.draw_box_plot()
+fig3.show()
+
+
+# In[ ]:
+
+
+
+
